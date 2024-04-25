4d0e88749980724cfecaf7636997a7aeb8269226
Vale <valeysa30@hotmail.com>
2024-04-25 19:34:20 +0200

    document functions

--- instruction_table.h
+++ instruction_table.h
@@ -13,7 +13,20 @@ struct instruction {
     int op3;
 };
 
+/*
+* @fn add_instruction
+* @brief Añade una instrucción a la tabla de instrucciones
+* @param inst_name Nombre de la instrucción
+* @param op1 Operando 1
+* @param op2 Operando 2
+* @param op3 Operando 3
+*/
 void add_instruction(char inst_name[], int op1, int op2, int op3);
+
+/*
+* @fn print_inst_tab
+* @brief Imprime la tabla de instrucciones
+*/
 void print_inst_tab();
 
 #endif
--- symbol_table.h
+++ symbol_table.h
@@ -5,21 +5,53 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <ctype.h>
 
 struct symbol {
     char id_name[128];
     int var_scope;
+    int value;
     // char type;
 };
 
-// Busca un símbolo en la tabla de símbolos
-bool search_symbol(char name[]);
+/*
+* @fn search_symbol
+* @brief Busca un símbolo en la tabla de símbolos y devuelve su índice, si no lo encuentra devuelve -1
+* @param name Nombre del símbolo a buscar
+* @return Índice del símbolo en la tabla de símbolos
+*/
+int search_symbol(char name[]);
 
-// Añade símbolo a la tabla de símbolos
+/*
+* @fn add_symbol
+* @brief Añade un símbolo a la tabla de símbolos
+* @param name Nombre del símbolo
+* @param val Valor del símbolo
+*/
 void add_symbol(char name[], int val);
+
+/*
+* @fn add_tmp_symbol
+* @brief Añade un símbolo temporal a la tabla de símbolos
+* @param name Nombre del símbolo
+* @param val Valor del símbolo
+*/
+void add_tmp_symbol(char name[], char val[]);
 
 int set_scope(int scope);
 
+/*
+* @fn is_valid_num
+* @brief Comprueba si un número es válido
+* @param num Número a comprobar
+* @return true si el número es válido, false en caso contrario
+*/
+bool is_valid_num(char num[]);
+
+/*
+* @fn print_tab
+* @brief Imprime la tabla de símbolos
+*/
 void print_tab();
 
 #endif
